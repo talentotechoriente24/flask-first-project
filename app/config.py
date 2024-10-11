4ab7cc76b8c3539433032060af7ac7073fb90bac
Deiber Olivasres <deiber.olivares@infinitytech.dev>
2024-10-10 20:18:36 -0500

    chore: Add SQLALCHEMY_ECHO flag to config

--- app/config.py
+++ app/config.py
@@ -7,5 +7,6 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super_secret_key'
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt_super_secret_key'
